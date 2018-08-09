e72ab4d96c38a835e18b410b3a6426bc48258805
Majid Chaudhary <majid.chaudhary@yahoo.co.uk>
2018-08-09 15:19:40 +0500

    11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { EmpComponent } from './emp/employee.component';
+import { EmpComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
