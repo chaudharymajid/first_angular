c4983da11c1b6dd0b74fa1d61f00b90ff51e37ff
chaudharymajid <majid.chaudhary@yahoo.co.uk>
2018-08-14 19:08:21 +0500

    angular pipes

--- src/app/employee/emp.complist.ts
+++ src/app/employee/emp.complist.ts
@@ -11,19 +11,19 @@ export class EmployeeComponentList {
 
     constructor(){
     this.employees = [
-        {code: '101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '25/04/1984'},
-        {code: '102', name: 'Andrew', gender: 'Male', annualSalary: 6500, dateOfBirth: '26/03/1985'},
-        {code: '103', name: 'Kate', gender: 'Female', annualSalary: 6000, dateOfBirth: '27/02/1986'},
-        {code: '104', name: 'Marry', gender: 'Female', annualSalary: 7000, dateOfBirth: '28/01/1987'},
+        {code: '101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '04/25/1984'},
+        {code: '102', name: 'Andrew', gender: 'Male', annualSalary: 6500, dateOfBirth: '03/26/1985'},
+        {code: '103', name: 'Kate', gender: 'Female', annualSalary: 6000, dateOfBirth: '02/27/1986'},
+        {code: '104', name: 'Marry', gender: 'Female', annualSalary: 7000, dateOfBirth: '01/28/1987'},
     ]};
 
     getEmployees():void{
         this.employees = [
-            {code: '101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '25/04/1984'},
-            {code: '102', name: 'Andrew', gender: 'Male', annualSalary: 6500, dateOfBirth: '26/03/1985'},
-            {code: '103', name: 'Kate', gender: 'Female', annualSalary: 6000, dateOfBirth: '27/02/1986'},
-            {code: '104', name: 'Marry', gender: 'Female', annualSalary: 7000, dateOfBirth: '28/01/1987'},
-            {code: '105', name: 'Nancy', gender: 'Female', annualSalary: 7100, dateOfBirth: '28/01/1989'},
+            {code: '101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '04/25/1984'},
+            {code: '102', name: 'Andrew', gender: 'Male', annualSalary: 6500, dateOfBirth: '03/26/1985'},
+            {code: '103', name: 'Kate', gender: 'Female', annualSalary: 6000, dateOfBirth: '02/27/1986'},
+            {code: '104', name: 'Marry', gender: 'Female', annualSalary: 7000, dateOfBirth: '01/28/1987'},
+            {code: '105', name: 'Nancy', gender: 'Female', annualSalary: 7100, dateOfBirth: '01/09/1901'},
         ]
     };
 
