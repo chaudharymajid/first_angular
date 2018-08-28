14edcf6b5058e554278fccf32e89c07ed6667c4c
chaudharymajid <majid.chaudhary@yahoo.co.uk>
2018-08-28 15:12:35 +0500

    cleanup

--- src/app/employee/emp.complist.ts
+++ src/app/employee/emp.complist.ts
@@ -11,10 +11,7 @@ export class EmployeeComponentList {
 
     constructor(){
     this.employees = [
-        {code: '101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '04/25/1984'},
-        {code: '102', name: 'Andrew', gender: 'Male', annualSalary: 6500, dateOfBirth: '03/26/1985'},
-        {code: '103', name: 'Kate', gender: 'Female', annualSalary: 6000, dateOfBirth: '02/27/1986'},
-        {code: '104', name: 'Marry', gender: 'Female', annualSalary: 7000, dateOfBirth: '01/28/1987'},
+        
     ]};
 
     getEmployees():void{
