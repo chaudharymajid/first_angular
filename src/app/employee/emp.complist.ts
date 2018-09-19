1aae5cae4956df17684cb7d0e7aa38ee1676ed66
chaudharymajid <majid.chaudhary@yahoo.co.uk>
2018-09-19 14:27:14 +0500

    toggle changed

--- src/app/employee/emp.complist.ts
+++ src/app/employee/emp.complist.ts
@@ -25,13 +25,9 @@ export class EmployeeComponentList implements OnInit {
 
     toggleEmployees(): void {
         this.showNewEmployee = !this.showNewEmployee;
-        if (this.employees.length > 4 || this.employees.length < 1) {
-            this.employees = [
-                { code: '101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '04/25/1984' },
-                { code: '102', name: 'Andrew', gender: 'Male', annualSalary: 6500, dateOfBirth: '03/26/1985' },
-                { code: '103', name: 'Kate', gender: 'Female', annualSalary: 6000, dateOfBirth: '02/27/1986' },
-                { code: '104', name: 'Marry', gender: 'Female', annualSalary: 7000, dateOfBirth: '01/28/1987' }
-            ]
+        if (this.employees.length  > 6) {
+            this._employeeService.getEmployees()
+            .subscribe((employeesData) => this.employees = employeesData);
         }
         else {
             this.employees = [
@@ -41,6 +37,7 @@ export class EmployeeComponentList implements OnInit {
                 { code: '104', name: 'Marry', gender: 'Female', annualSalary: 7000, dateOfBirth: '01/28/1987' },
                 { code: '105', name: 'Nancy', gender: 'Female', annualSalary: 7100, dateOfBirth: '01/09/1901' },
                 { code: '106', name: 'Harry', gender: 'Male', annualSalary: 5400, dateOfBirth: '03/24/1983' },
+                { code: '107', name: 'Adam', gender: 'Male', annualSalary: 7500, dateOfBirth: '04/02/2009' },
             ]
         }
     };
