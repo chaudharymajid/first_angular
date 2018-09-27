2e614cccfd47dc9ea5170cd8ebdcbf01160be710
chaudharymajid <majid.chaudhary@yahoo.co.uk>
2018-09-27 10:07:07 +0500

    failed tries bootstrap menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,29 @@ import { HttpModule } from '@angular/http';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { EmployeeComponentList } from './employee/emp.complist';
 import { EmployeeTitlePipe } from './employee/employeeTitle.pipe';
 import { EmployeeCountComponent } from './employee/employeeCount.component';
+import { homeComponent } from './home/home.component';
+import { pageNotFound } from './others/pageNotFound.component';
+
+const appRoutes: Routes = [
+  {path: 'home', component: homeComponent},
+  {path: 'employees', component: EmployeeComponentList},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: pageNotFound}
+]
 
 @NgModule({
   declarations: [
-    AppComponent , EmployeeComponent, EmployeeComponentList, EmployeeTitlePipe, EmployeeCountComponent
+    AppComponent , EmployeeComponent, EmployeeComponentList, EmployeeTitlePipe, EmployeeCountComponent, homeComponent, pageNotFound
   ],
   imports: [
-    BrowserModule, FormsModule, HttpModule, BsDropdownModule, TooltipModule, ModalModule
+    BrowserModule, FormsModule, HttpModule, BsDropdownModule, TooltipModule, ModalModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/others/pageNotFound.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    template: `<h1> 404 - Page not found</h1>`
+})
+
+export class pageNotFound{
+}
